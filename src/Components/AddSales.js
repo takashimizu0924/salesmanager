fe78687b7c695928dfc4cea1000e8a94cb716b6a
NoneHuman <desperatexxxxxxdestruction@gmail.com>
2021-03-14 20:07:11 +0900

    登録処理対応

--- src/Components/AddSales.js
+++ src/Components/AddSales.js
@@ -42,6 +42,7 @@ const useStyles = makeStyles({
     },
 });
 
+/*
 const rows = [
     { id:0,date:'2021/3/01',recipetNumber:'1017402004005',name:'清水',workItem:'2.2kﾋｮｳｼﾞｭﾝｺｳｼﾞ',quantity:1,price:'7700'},
     { id:0,date:'2021/3/01',recipetNumber:'1017402004005',name:'清水',workItem:'2.2kﾋｮｳｼﾞｭﾝｺｳｼﾞ',quantity:1,price:'7700'},
@@ -58,7 +59,18 @@ const rows = [
     { id:0,date:'2021/3/01',recipetNumber:'1017402004005',name:'清水',workItem:'2.2kﾋｮｳｼﾞｭﾝｺｳｼﾞ',quantity:1,price:'7700'},
 
 ];
+*/
 
+var quantity_clickFlg = false;
+var id_data = 0;
+var date_data;
+var recipe_data;
+var name_data;
+var workItem_data;
+var quantity_data = 0;
+var price_data = 0;
+
+var rows = [];
 
 const workItems = [
     {
@@ -86,8 +98,19 @@ const workItems = [
 
 var workName = Data.data.work.root
 
-// 
+// 情報初期化処理
+const GlobalBufferInit = () => {
+    quantity_clickFlg = false;
+    quantity_data = 0;
+    price_data = 0;
+}
+
+// 工事内容判別用関数
 const WorkContents = (workItem) => {
+    var result_map_data;
+    // グローバル変数に格納
+    workItem_data = workItem;
+
     if (workItem === 'エアコン工事'){
         workName = Data.data.work.エアコン工事
 
@@ -97,68 +120,89 @@ const WorkContents = (workItem) => {
     }else{
         workName = Data.data.work.other
     }
+
     return workName
 }
 
-const SelectWork = (workname) => {
+// Price情報取得関数
+const GetPriceData = (quantity, arg_name, arg_contents) => {
+    // Json解析オブジェクト生成
+    const obj_from_root = Data.data;
+    var obj_name = obj_from_root.work[arg_name];
+    var property;
+    var calc_data;
+    var flag = GetPriceFlag();
+    quantity_data = quantity;
     
-    const [work, setWork] = React.useState('');
-    const handleWorkChange = (event) => {
-        setWork(event.target.value)
-    };
-    return (
+    for(var idx in obj_name){
+        property = Object.keys(obj_name[idx]);
+        for(var k in property){
+            if(obj_name[idx][property[k]] == arg_contents){
+                price_data = obj_name[idx][property[++k]];
+                console.log(obj_name[idx][property[k]]);
+                console.log(obj_name[idx][property[++k]]);
+                break;
+            }
+        }
+    }
+    
+    if(flag == false){
+        calc_data = price_data * quantity_data;
+        console.log(flag);
+    }else{
+        SetPriceFlag(false);
+        calc_data = price_data;
+        console.log(flag);
+    }
 
-        <TextField
-            variant="outlined"
-            fullWidth
-            select
-            value={work}
-            onInput={e => setWork(e.target.value)}
-            onChange={handleWorkChange}
-            >
-                {workName.map((val) =>(
-                    <MenuItem key = {val.name} value={val.name}>
-                        {val.name}
-                    </MenuItem>
-                ))}
-            </TextField>
-    )
+    console.log(obj_name);
+    //console.log(obj_price);
+    console.log(arg_name);
+    console.log(arg_contents);
+
+    return calc_data;
 }
 
-const SelectWorkItem = () => {
-    const [workItem, setWorkItem] = React.useState("");
-    const handleChange = (event) => {
-        setWorkItem(event.target.value) 
-    };
-    return(
-        <TextField
-            variant="outlined"
-            fullWidth
-            select
-            label="セレクト"
-            value={workItem}
-            onChange={handleChange}>
-                {workItems.map((val) => (
-                <MenuItem key={val.value} value={val.value} >
-                    {val.label}
-                </MenuItem>
-    ))}
-        </TextField>
-    )
-};
+// 金額入力フラグ設定関数
+const SetPriceFlag = (flg) => {
+    return quantity_clickFlg = flg;
+}
+
+// 金額入力フラグ取得関数
+const GetPriceFlag = () => {
+    return quantity_clickFlg;
+}
 
 const AddSales = () => {
     const mainData = {}
     const AddItem = () => {
-        console.log(name,workItem,work)
+        var data_object = {
+            id:id_data,
+            date:date_data,
+            recipetNumber:recipe_data,
+            name:name_data,
+            workItem:workItem_data,
+            quantity:quantity_data,
+            price:price_data
+        };
+
+        id_data++;
+        // console.log(name,workItem,work);
+        console.log(workItem,work);
+        console.log(id_data,date_data,recipe_data,name_data,workItem_data,quantity_data,price_data);
+        rows.push(data_object);
+        GlobalBufferInit();
     };
     const classes = useStyles();
     const { handleSubmit } = useForm();
     const [name, setName ] = React.useState();
     const [workItem, setWorkItem] = React.useState("");
-    const handleChange = (event) => {setWorkItem(event.target.value)};
     const [work, setWork] = React.useState("");
+    const [price, setPrice] = React.useState("");
+
+    const handleChange = (event) => {setWorkItem(event.target.value)};
     const handleWorkChange = (event) => {setWork(event.target.value)};
+    const handlePriceFlgChange = () => {SetPriceFlag(true)};
     
     React.useEffect(() => {
 
@@ -177,14 +221,14 @@ const AddSales = () => {
                                 <Grid container spacing={2} justify="flex-end">
                                     <Grid item xs={3} >
                                         {/* <DatePicker /> */}
-                                        <TextField type="date" fullWidth label="完了日" variant="outlined">
+                                        <TextField type="date" onInput={e => date_data = e.target.value} variant="outlined">
                                         </TextField>
                                     </Grid>
                                     <Grid item xs={4}>
-                                        <TextField fullWidth label="伝票番号" variant="outlined"></TextField>
+                                        <TextField onInput={e => recipe_data = e.target.value} fullWidth label="伝票番号" variant="outlined"></TextField>
                                     </Grid>
                                     <Grid item xs={5}>
-                                        <TextField onInput={e => setName(e.target.value)} fullWidth label="お客様名" variant="outlined"></TextField>
+                                        <TextField onInput={e => name_data = e.target.value} fullWidth label="お客様名" variant="outlined"></TextField>
                                     </Grid>
                                     <Grid item xs={4}>
                                     <TextField
@@ -210,9 +254,7 @@ const AddSales = () => {
                                         label="工事内容"
                                         value={work}
                                         onInput={e => setWork(e.target.value)}
-                                        onChange={handleWorkChange}
-                                        >
-                                            
+                                        onChange={handleWorkChange}>
                                             {WorkContents(workItem).map((val) =>(
                                                 <MenuItem key = {val.name} value={val.name}>
                                                     {val.name}
@@ -222,10 +264,20 @@ const AddSales = () => {
                                         {/* <SelectWork /> */}
                                     </Grid>
                                         <Grid item xs={3}>
-                                            <TextField fullWidth label="数量" variant="outlined"></TextField>
+                                            <TextField onInput={e => price_data = GetPriceData(e.target.value, workItem, work)} fullWidth label="数量" variant="outlined"></TextField>
                                         </Grid>
                                         <Grid item xs={4}>
-                                            <TextField fullWidth label="金額" variant="outlined"></TextField>
+                                            {/* {WorkContents(workItem).map((val) =>(          
+                                            
+                                            ))} */}
+                                            <TextField
+                                                fullWidth label="金額" 
+                                                //value={price_data}
+                                                onClick={handlePriceFlgChange} 
+                                                onInput={e => price_data = e.target.value}
+                                                variant="outlined">
+                                            </TextField>
+                                            
                                         </Grid>
                                         <Grid item xs={2} alignContent="center" alignItems="center">
                                             <Button variant="outlined" color="primary" size="large" type="submit">登録</Button>
